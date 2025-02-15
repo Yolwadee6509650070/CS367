a8bb8fe265635e79fdb772063ce0ffcafd7db07c
Yolwadee6509650070 <144804039+Yolwadee6509650070@users.noreply.github.com>
2025-02-16 00:27:00 +0700

    change name

---
+++ src/test/java/dev/natthida/cdloan/CDLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.natthida.cdloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CDLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
